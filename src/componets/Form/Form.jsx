cc70f567467bf52a3054da7030a2f2931cca67bf
adi927 <jaskolskiadam41@gmail.com>
2023-12-23 22:56:55 +0100

    Minor corrections

--- src/componets/Form/Form.jsx
+++ src/componets/Form/Form.jsx
@@ -25,29 +25,32 @@ const Form = ({ events, setEvents, isError, setIsError }) => {
   return (
     <div className={styles.wrapper}>
       <p className={styles.p}>DODAJ WYDARZENIE</p>
-      <div>
+      <div className={styles.inputs}>
         <input
           type="text"
           className={styles.eventInput}
           placeholder="Napisz wydarzenie"
           value={name}
           onChange={(event) => setName(event.target.value)}
-        ></input>
+          maxLength="1000"
+        />
         <input
           type="text"
           className={styles.dateInput}
           placeholder="Napisz date"
           value={date}
           onChange={(event) => setDate(event.target.value)}
-        ></input>
+        />
       </div>
       <input
+        required
+        maxLength="1000"
         type="text"
         className={styles.descriptionInput}
         placeholder="Napisz opis"
         value={description}
         onChange={(event) => setDescription(event.target.value)}
-      ></input>
+      />
       <button onClick={AddProduct} className={styles.addButton}>
         <p className={styles.pBtn}>DODAJ</p>
       </button>
--- src/componets/Main/Main.jsx
+++ src/componets/Main/Main.jsx
@@ -1,5 +1,4 @@
 import EventLog from "../EventLog/EventLog";
-import Form from "../Form/Form";
 import styles from "./Main.module.css";
 
 const Main = () => {
